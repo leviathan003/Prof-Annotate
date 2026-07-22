26a6e395cbd348ec1df77e05104eec63b8666d7e
rohit <rohitslg.003@gmail.com>
2026-07-22 23:07:06 +0530

    update: flow of kpt selection changed to directly choose from the kpt selector window so that there is no need for providing kpt counts beforehand. Annotator selects as many as they need, data.yaml is updated accordingly

--- profannotate/config/constants.py
+++ profannotate/config/constants.py
@@ -15,7 +15,7 @@ else:
     _PACKAGE_ROOT = Path(__file__).resolve().parent.parent.parent
 
 APP_NAME = "Prof Annotate"
-APP_VERSION = "1.0.13"
+APP_VERSION = "1.1.0"
 APP_DOCS_URL = "https://profannotate.readthedocs.io"
 
 ASSETS_DIR = _PACKAGE_ROOT / "assets"
